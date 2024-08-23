180fe5e36e15bb990f7381c70d69d62f04e939ba
Juan Villafuerte Mollinedo <villafuerte.j@outlook.com>
2024-08-23 13:59:54 -0400

    O by Open/closed principle (part I)

--- 2-OpenClose/EmployeeContractTime.cs
+++ 2-OpenClose/EmployeeContractTime.cs
@@ -1,8 +1,8 @@
 namespace OpenClose
 {
-    public class EmployeeFullTime : Employee
+    public class EmployeeContractTime : Employee
     {
-        public EmployeeFullTime(string fullname, int hoursWorked)
+        public EmployeeContractTime(string fullname, int hoursWorked)
         {
             Fullname = fullname;
             HoursWorked = hoursWorked;
@@ -10,7 +10,7 @@ namespace OpenClose
 
         public override decimal CalculateSalaryMonthly()
         {
-            decimal hourValue = 30000M;
+            decimal hourValue = 10000M;
             decimal salary = hourValue * HoursWorked;
             return salary;
         }
